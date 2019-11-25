280a420016f64866e80a9171c6361874dc8f2c91
mloureiro93 <mariamadureiraloureiro@gmail.com>
2019-11-25 17:36:49 +0000

    create post

---
+++ ProjectTwo/2daysaway/models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      maxlength: 500,
+      minlength: 1
+    },
+    author: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    images: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Image'
+      }
+    ]
+  },
+  {
+    timestamps: {
+      createdAt: 'creationDate',
+      updatedAt: 'updateDate'
+    }
+  }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
--- ProjectTwo/2daysaway/routes/index.js
+++ ProjectTwo/2daysaway/routes/index.js
@@ -1,28 +1,31 @@
-const {
-  Router
-} = require('express');
+const { Router } = require("express");
 const router = new Router();
 
-router.get('/', (req, res, next) => {
-  res.render('homepage', {
-    title: '2Days Away'
+router.get("/", (req, res, next) => {
+  res.render("homepage", {
+    title: "2Days Away"
   });
 });
 
 //REVIEW POSTS
 
-router.get('/reviews', (req, res, next) => {
-  res.render('Post/listPosts')
+router.get("/reviews", (req, res, next) => {
+  res.render("Post/listPosts");
 });
 
-router.get('/singlePost', (req, res, next) => {
-  res.render('Post/singlePost')
+router.get("/createPost", (req, res, next) => {
+  res.render("Post/createPost");
 });
 
-router.get('/userprofile', (req, res, next) => {
-  res.render('user')
+router.post("/createPost", (req, res, next) => {
+  console.log("----here-----");
+  const name = req.body.text;
+  console.log("HERE -> REQ.BODY-------", name);
+  res.redirect("/userprofile");
 });
 
-
+router.get("/userprofile", (req, res, next) => {
+  res.render("user");
+});
 
 module.exports = router;
