10982fd1145df7b3d4b38f9254f8aa71d2ad43cc
mloureiro93 <mariamadureiraloureiro@gmail.com>
2019-11-25 10:05:04 +0000

    changed views and routes to day 1 and 2

--- ProjectTwo/2daysaway/routes/user.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = new Router();
-
-router.get('/', (req, res, next) => {
-  res.render('user', { name: 'James Dean' });
-});
-
-module.exports = router;
