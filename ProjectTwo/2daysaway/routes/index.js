5ff70f62525d51a2bb56e8898b9f50ddc940d4c4
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2019-11-26 17:39:20 +0000

    bootstrap incorporation.

--- ProjectTwo/2daysaway/routes/index.js
+++ ProjectTwo/2daysaway/routes/index.js
@@ -1,6 +1,4 @@
-const {
-  Router
-} = require("express");
+const { Router } = require("express");
 const router = new Router();
 
 function randomIndex(array) {
@@ -9,11 +7,11 @@ function randomIndex(array) {
 const Poi = require("./../models/POI");
 // const cities = require("./../bin/itinerary");
 
-const Post = require('./../models/post');
+const Post = require("./../models/post");
 
-const routeGuard = require('./../middleware/route-guard');
+const routeGuard = require("./../middleware/route-guard");
 
-const uploader= require('./../middleware/cloudinary-config');
+const uploader = require("./../middleware/cloudinary-config");
 
 router.get("/", (req, res, next) => {
   res.render("homepage", {
@@ -21,32 +19,18 @@ router.get("/", (req, res, next) => {
   });
 });
 
-
 //user profile
 
 router.get("/userprofile", (req, res, next) => {
   res.render("user");
 });
 
-
-<<<<<<< HEAD
-//Routes for Itinerary (Day1 & Day2)
-router.get("/day1", (req, res, next) => {
-  Poi.find().then(result => {
-    res.render("Itinerary/day1", result[0]);
-  });
-=======
-
-
-
 //routes for selectionPage
 router.get("/tripSelection", (req, res, next) => {
   res.render("Itinerary/tripSelection");
->>>>>>> 987bd5b8473e4f4be7de843e17ad64c9eb3fb363
 });
 
 router.post("/tripSelection", (req, res, next) => {
-
   const city = req.body.travelingFrom;
   let filteredArray;
   Poi.find({}).then(cities => {
@@ -62,23 +46,20 @@ router.post("/tripSelection", (req, res, next) => {
 router.get("/day1/:id", (req, res, next) => {
   const cityId = req.params.id;
   // TODO: GET THE VALUE OF "id", SEARCH IT IN YOUR DATABASE AND SEND IT TO YOUR VIEW5
-  Poi.findById(cityId)
-    .then(city => {
-      res.render("Itinerary/day1", {
-        city
-      });
+  Poi.findById(cityId).then(city => {
+    res.render("Itinerary/day1", {
+      city
     });
+  });
 });
 
 router.get("/day2/:id", (req, res, next) => {
   const cityId = req.params.id;
-  Poi.findById(cityId)
-    .then(city => {
-      res.render("Itinerary/day2", {
-        city
-      });
+  Poi.findById(cityId).then(city => {
+    res.render("Itinerary/day2", {
+      city
     });
+  });
 });
-
 
 module.exports = router;
